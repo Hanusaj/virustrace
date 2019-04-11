df300d5b6bc5aa5ba70cd256bf64c6f52f672e72
AJ Hanus <hanusaj@gmail.com>
2019-04-10 23:56:34 -0500

    Initial files.

---
+++ src/CommunicationsMonitor.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+class CommunicationsMonitor {
+
+    CommunicationsMonitor() {
+
+    }
+
+    void addCommunication(int c1, int c2, int timestamp) {
+        System.out.println("Need to implement adding communications.");
+        return;
+    }
+
+    void createGraph() {
+
+    }
+
+    List<ComputerNode> queryInfection(int c1, int c2, int x, y) {
+        
+    }
+
+    public static void main(String[] args) {
+        CommunicationsMonitor cm = new CommunicationsMonitor();
+        cm.addCommunication(1, 2, 5);
+    }
+
+}
