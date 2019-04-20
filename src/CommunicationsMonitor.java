7fc366e21f563007cef524d28f8d3e7002f0d6b4
AJ Hanus <hanusaj@gmail.com>
2019-04-20 14:16:39 -0500

    Added triples list.

--- src/CommunicationsMonitor.java
+++ src/CommunicationsMonitor.java
@@ -1,25 +1,29 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 class CommunicationsMonitor {
 
     HashMap<Integer, List<ComputerNode>> computers;
+    ArrayList<int[]> triples;
 
     CommunicationsMonitor() {
         this.computers = new HashMap<Integer, List<ComputerNode>>();
     }
 
     void addCommunication(int c1, int c2, int timestamp) {
-        ComputerNode cn1 = new ComputerNode(c1, timestamp);
-        ComputerNode cn2 = new ComputerNode(c2, timestamp);
-        cn1.addEdge(cn2);
-        cn2.addEdge(cn1);
-        return;
+        int[] triple = {c1, c2, timestamp};
+        triples.add(triple);
     }
 
     void createGraph() {
-
+        triples.sort();
+        for(int i = 0; i < triples.size(); i++) {
+            int[] currentTriple = triples.get(i);
+            ComputerNode cn1 = new ComputerNode(currentTriple[0], currentTriple[2]);
+            ComputerNode cn2 = new ComputerNode(currentTriple[1], currentTriple[2]);
+        }
     }
 
     List<ComputerNode> queryInfection(int c1, int c2, int x, int y) {
@@ -45,8 +49,20 @@ class CommunicationsMonitor {
     }
 
     public static void main(String[] args) {
-        CommunicationsMonitor cm = new CommunicationsMonitor();
-        cm.addCommunication(1, 2, 5);
+		int compMap[][] = {{1, 4, 12}, {1, 2, 4}, {2, 4, 8}, {3, 4, 8}};
+		CommunicationsMonitor cMon = new CommunicationsMonitor();
+		for(int i = 0; i<compMap.length; i++) {
+			cMon.addCommunication(compMap[i][0], compMap[i][1], compMap[i][2]);
+        }
+		cMon.createGraph();
+		HashMap<Integer, List<ComputerNode>> compMapping = cMon.getComputerMapping();
+		
+		//Check Size
+        System.out.println("Number nodes: " + compMapping.size());
+		
+		//Check for node 4
+		// String msg = checkCNList(compMapping.get(4), new int[][]{{4, 8},{4, 12}});
+		// if(!msg.equals("")) fail("Incorrect list in HashMap for key 4, "+msg);
     }
 
 }
